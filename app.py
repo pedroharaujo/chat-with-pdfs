16fc9dd9d494506649bb6516fb87ca26d6a7e2d2
pedro.araujo <pedro.araujo@brius.com.br>
2023-07-31 03:02:26 -0300

    feat: editing persisten text layout and embedding models

--- app.py
+++ app.py
@@ -6,13 +6,27 @@ from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
-from langchain.llms import OpenAI
+from langchain.llms import OpenAI, HuggingFaceHub
 from langchain.chat_models import ChatOpenAI
 from htmlTemplates import css, bot_template, user_template
 
 
+def handle_user_input(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+
+
 def get_conversation_chain(vectorstore):
-    llm = ChatOpenAI()
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo")
+    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl",
+    #                      model_kwargs={"temperature": 0.5, "max_length": 512})
     memory = ConversationBufferMemory(
         memory_key="chat_history", return_messages=True)
     return ConversationalRetrievalChain.from_llm(
@@ -21,9 +35,9 @@ def get_conversation_chain(vectorstore):
 
 
 def get_vectorstore(text_chunks):
-    embeddings = OpenAIEmbeddings()
-    # embeddings = HuggingFaceInstructEmbeddings(
-    #     model_name="hkunlp/instructor-xl")
+    # embeddings = OpenAIEmbeddings()
+    embeddings = HuggingFaceInstructEmbeddings(
+        model_name="hkunlp/instructor-xl")
     return FAISS.from_texts(texts=text_chunks, embedding=embeddings)
 
 
@@ -53,15 +67,15 @@ def main():
     st.write(css, unsafe_allow_html=True)
 
     if "conversation" not in st.session_state:
-        st.session_state.conversatino = None
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
 
     st.header("Chat with multiples PDFs :books:")
-    st.text_input("Ask me a question about your PDF documents:")
-
-    st.write(user_template.replace(
-        "{{MSG}}", "Hello robot"), unsafe_allow_html=True)
-    st.write(bot_template.replace(
-        "{{MSG}}", "Hello human"), unsafe_allow_html=True)
+    user_question = st.text_input(
+        "Ask me a question about your PDF documents:")
+    if user_question:
+        handle_user_input(user_question)
 
     with st.sidebar:
         st.subheader("Your documents")
